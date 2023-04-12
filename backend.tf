2d56c229b1fdfda29708957f0d4425d7880963d3
Wiola <wioleta.drobik@gmail.com>
2023-04-12 19:34:13 +0200

    feat: comment out backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_version = ">= 1.0.11"
-  backend "gcs" {
-    bucket = "wd-ml-terraform-state-file"
-    prefix = "dev"
-  }
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.41.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "4.41.0"
-    }
-  }
-}
+#terraform {
+#  required_version = ">= 1.0.11"
+#  backend "gcs" {
+#    bucket = "wd-ml-terraform-state-file"
+#    prefix = "dev"
+#  }
+#  required_providers {
+#    google = {
+#      source  = "hashicorp/google"
+#      version = "4.41.0"
+#    }
+#    google-beta = {
+#      source  = "hashicorp/google-beta"
+#      version = "4.41.0"
+#    }
+#  }
+#}
