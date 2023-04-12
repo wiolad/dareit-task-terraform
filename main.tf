7a172a045201d8df690d41afad7a400f8dbc8340
Wiola <wioleta.drobik@gmail.com>
2023-04-12 19:31:39 +0200

    feat: comment out the Terraform code

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
-resource "google_storage_bucket" "static" {
- project       = "weighty-replica-378016"
- name          = "bucket-tf-wd-ci"
- location      = "europe-central2"
- storage_class = "STANDARD"
-}
+# resource "google_storage_bucket" "static" {
+# project       = "weighty-replica-378016"
+# name          = "bucket-tf-wd-ci"
+# location      = "europe-central2"
+# storage_class = "STANDARD"
+#}
 
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+#resource "google_compute_instance" "dareit-vm-ci" {
+#  name         = "dareit-vm-tf-ci"
+#  machine_type = "e2-medium"
+#  zone         = "us-central1-a"
+#
+#  tags = ["dareit"]
 
-  tags = ["dareit"]
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11"
+#      labels = {
+#        managed_by_terraform = "true"
+#      }
+#    }
+#  }
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
+#  network_interface {
+#    network = "default"
+#
+#    access_config {
+#      // Ephemeral public IP
+#    }
+#  }
+#}
